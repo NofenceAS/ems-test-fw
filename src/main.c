4a465940b113353e731148d76b0c84573fd1a91e
eivind.haldorsen <eivind.haldorsen@inventas.no>
2021-12-03 03:42:15 -0800

    XF-52 add event manager init in main

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "ble/nf_ble.h"
 #include <sys/printk.h>
 #include <zephyr.h>
+#include <event_manager.h>
 
 /**
  * The Nofence X3 main entry point. This is
@@ -11,4 +12,9 @@ void main(void)
 {
 	printk("main %p\n", k_current_get());
 	nf_ble_init();
+    /* Initialize the event manager to have modules subscribed to respective topics */
+    int err = event_manager_init();
+    if (err) {
+        // Error handler
+    }
 }
