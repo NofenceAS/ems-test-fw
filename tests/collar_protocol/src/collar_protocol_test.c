28a60146b893a33e304e8db26b484d0b900c217a
Marius Lind Volstad <marius@vec.no>
2021-12-13 15:21:21 +0100

    XF-57 Correcting collar protocol test style of comments and indentation.

--- tests/collar_protocol/src/collar_protocol_test.c
+++ tests/collar_protocol/src/collar_protocol_test.c
@@ -60,13 +60,13 @@ static void test_decode_valid_data(void)
 }
 
 static void test_invalid_input(void) {
-	// Valid protobuf data of firmware_upgrade_req
+	/* Valid protobuf data of firmware_upgrade_req */
 	int result;
 	size_t raw_size = 0;
 	uint8_t raw_buf[] = {0x0A, 0x0C, 0x08, 0xB9, 0x0A, 0x10, 0xD4, 0xCB, 0xC4, 0x8D, 0x06, 0x18, 0xC7, 0x02, 0x22, 0x10, 0x08, 0xBE, 0xDE, 0x38, 0x10, 0x10, 0x18, 0x0E, 0x22, 0x06, 0x08, 0x0C, 0x10, 0x00, 0x18, 0x01};
 	NofenceMessage message;
 	
-	// Test decode with invalid input arguments
+	/* Test decode with invalid input arguments */
 	result = collar_protocol_decode(raw_buf, sizeof(raw_buf), NULL);
 	zassert_equal(result, -EINVAL, "collar_protocol_decode returned wrong error for invalid arguments");
 	result = collar_protocol_decode(raw_buf, 0, &message);
@@ -82,11 +82,11 @@ static void test_invalid_input(void) {
 	result = collar_protocol_decode(NULL, 0, NULL);
 	zassert_equal(result, -EINVAL, "collar_protocol_decode returned wrong error for invalid arguments");
 
-	// Decode raw_buf, should work since it contains valid data and was tested previously
+	/* Decode raw_buf, should work since it contains valid data and was tested previously */
 	result = collar_protocol_decode(raw_buf, sizeof(raw_buf), &message);
 	zassert_equal(result, 0, "collar_protocol_decode returned unexpected error");
 
-	// Test encode with invalid input arguments
+	/* Test encode with invalid input arguments */
 	result = collar_protocol_encode(&message, raw_buf, sizeof(raw_buf), NULL);
 	zassert_equal(result, -EINVAL, "collar_protocol_encode returned wrong error for invalid arguments");
 	result = collar_protocol_encode(&message, raw_buf, 0,               &raw_size);
@@ -124,22 +124,22 @@ static void test_buffer_overflow(void) {
 	NofenceMessage message;
 	size_t raw_size = 0;
 
-	// Valid protobuf data of firmware_upgrade_req
+	/* Valid protobuf data of firmware_upgrade_req */
 	uint8_t raw_buf[] = {0x0A, 0x0C, 0x08, 0xB9, 0x0A, 0x10, 0xD4, 0xCB, 0xC4, 0x8D, 0x06, 0x18, 0xC7, 0x02, 0x22, 0x10, 0x08, 0xBE, 0xDE, 0x38, 0x10, 0x10, 0x18, 0x0E, 0x22, 0x06, 0x08, 0x0C, 0x10, 0x00, 0x18, 0x01};
 
-	// Decode raw_buf, should work since it contains valid data and was tested previously
+	/* Decode raw_buf, should work since it contains valid data and was tested previously */
 	result = collar_protocol_decode(raw_buf, sizeof(raw_buf), &message);
 	zassert_equal(result, 0, "collar_protocol_decode failed for valid data");
 
-	// Attempt encode with buffer size one less than required
+	/* Attempt encode with buffer size one less than required */
 	result = collar_protocol_encode(&message, raw_buf, sizeof(raw_buf)-1, &raw_size);
 	zassert_equal(result, -EMSGSIZE, "collar_protocol_encode returned wrong result for insufficient buffer size (-1)");
 
-	// Attempt encode with buffer size half of required
+	/* Attempt encode with buffer size half of required */
 	result = collar_protocol_encode(&message, raw_buf, sizeof(raw_buf)/2, &raw_size);
 	zassert_equal(result, -EMSGSIZE, "collar_protocol_encode returned wrong result for insufficient buffer size (/2)");
 
-	// Attempt encode with buffer size of 1
+	/* Attempt encode with buffer size of 1 */
 	result = collar_protocol_encode(&message, raw_buf, 1, &raw_size);
 	zassert_equal(result, -EMSGSIZE, "collar_protocol_encode returned wrong result for insufficient buffer size (=1)");
 }
@@ -147,7 +147,7 @@ static void test_buffer_overflow(void) {
 static void test_corrupted_decode(void) {
 	int result;
 	NofenceMessage message;
-	// Attempt decode with corrupted buffer data
+	/* Attempt decode with corrupted buffer data */
 	uint8_t corrupted_raw_buf[] = {0xFF, 0xFE, 0x99, 0x78, 0x0A, 0x10, 0xD4, 0xFF, 0xC4, 0x8D, 0x00, 0x18, 0xC7, 0x02, 0xAA, 0x10, 0x08, 0xBE, 0xDE, 0x38, 0x10, 0x10, 0x18, 0x0E, 0x22, 0x06, 0x08, 0x0C, 0x10, 0x00, 0x18, 0x01};
 	
 	result = collar_protocol_decode(corrupted_raw_buf, sizeof(corrupted_raw_buf), &message);
@@ -157,12 +157,12 @@ static void test_corrupted_decode(void) {
 
 void test_main(void)
 {
-        ztest_test_suite(common,
-                         ztest_unit_test(test_encode_valid_data),
-                         ztest_unit_test(test_decode_valid_data),
-                         ztest_unit_test(test_invalid_input),
-                         ztest_unit_test(test_buffer_overflow),
-                         ztest_unit_test(test_corrupted_decode)
-                         );
-        ztest_run_test_suite(common);
+	ztest_test_suite(common,
+			ztest_unit_test(test_encode_valid_data),
+			ztest_unit_test(test_decode_valid_data),
+			ztest_unit_test(test_invalid_input),
+			ztest_unit_test(test_buffer_overflow),
+			ztest_unit_test(test_corrupted_decode)
+			);
+	ztest_run_test_suite(common);
 }
