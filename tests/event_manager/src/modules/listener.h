eabf3d58c9534c6a185f4d5794d144d838fa95f9
eivind.haldorsen <eivind.haldorsen@inventas.no>
2021-12-14 01:33:40 -0800

    XF-61 cleanup test files for event manager

--- tests/event_manager/src/modules/listener.h
+++
@@ -1,7 +0,0 @@
-#include <event_manager.h>
-
-/**
- * @brief Sends a request to the publisher module (simulated sensor) that
- * the simulated sensor should update its contents to the event bus
- */
-void request_sensor_data();
