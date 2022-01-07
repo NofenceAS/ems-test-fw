57e843ac49b844f353920c8b23b5de49c88ea366
Daniel Svendsen <daniel.svendsen@inventas.no>
2022-01-07 12:06:26 +0100

    (ble_nus) merge in changes from main

--- src/main.c
+++ src/main.c
@@ -2,7 +2,6 @@
 #include "ble/nf_ble.h"
 #include <sys/printk.h>
 #include <zephyr.h>
-<<<<<<< HEAD
 #include <logging/log.h>
 #include <event_manager.h>
 
@@ -10,9 +9,6 @@
 #include "module_state_event.h"
 
 LOG_MODULE_REGISTER(MODULE);
-=======
-#include <event_manager.h>
->>>>>>> main
 
 /**
  * The Nofence X3 main entry point. This is
@@ -20,20 +16,10 @@ LOG_MODULE_REGISTER(MODULE);
  */
 void main(void)
 {
-<<<<<<< HEAD
 	LOG_INF("Starting nofence application");
 	if (event_manager_init()) {
 		LOG_ERR("Event manager not initialized");
 	} else {
 		module_set_state(MODULE_STATE_READY);
 	}
-=======
-	printk("main %p\n", k_current_get());
-	nf_ble_init();
-    /* Initialize the event manager to have modules subscribed to respective topics */
-    int err = event_manager_init();
-    if (err) {
-        // Error handler
-    }
->>>>>>> main
 }
