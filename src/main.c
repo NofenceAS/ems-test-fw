9b24009b46ae16570f142b46b64cd97123d04696
pshustad <psh@axbit.com>
2022-01-07 15:55:13 +0100

    XF-68 : basic EEPROM persistence API

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 
 #include "ble/nf_ble.h"
+#include "nf_eeprom.h"
 #include <sys/printk.h>
 #include <zephyr.h>
 #include <event_manager.h>
+#include <devicetree.h>
 
 /**
  * The Nofence X3 main entry point. This is
@@ -12,9 +14,11 @@ void main(void)
 {
 	printk("main %p\n", k_current_get());
 	nf_ble_init();
-    /* Initialize the event manager to have modules subscribed to respective topics */
-    int err = event_manager_init();
-    if (err) {
-        // Error handler
-    }
+	const struct device *eeprom_dev = DEVICE_DT_GET(DT_ALIAS(eeprom));
+	eep_init(eeprom_dev);
+	/* Initialize the event manager to have modules subscribed to respective topics */
+	int err = event_manager_init();
+	if (err) {
+		// Error handler
+	}
 }
