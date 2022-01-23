272141aec2658c41fda17c6a7dc20359d8077f82
Marius Lind Volstad <marius@vec.no>
2022-01-23 18:56:06 +0100

    Implemented command parser for RTT and BLE.

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <event_manager.h>
 #include <logging/log.h>
 #include <sys/printk.h>
+#include <drivers/gpio.h>
 #include <zephyr.h>
 
 #include "diagnostics.h"
@@ -17,6 +18,9 @@
 
 LOG_MODULE_REGISTER(MODULE, CONFIG_LOG_DEFAULT_LEVEL);
 
+static const struct device *gpio0_dev;
+static const struct device *gpio1_dev;
+
 /**
  * The Nofence X3 main entry point. This is
  * called from the Zephyr kernel.
@@ -26,6 +30,17 @@ void main(void)
 	LOG_INF("Starting Nofence application");
 	const struct device *eeprom_dev = DEVICE_DT_GET(DT_ALIAS(eeprom));
 	eep_init(eeprom_dev);
+
+	////////////////////////////////////////////////////
+	// Set safe default states for pins
+	////////////////////////////////////////////////////
+	gpio0_dev = device_get_binding(DT_LABEL(DT_NODELABEL(gpio0)));
+	gpio1_dev = device_get_binding(DT_LABEL(DT_NODELABEL(gpio1)));
+
+	int err = gpio_pin_configure(gpio0_dev, 13, GPIO_OUTPUT_LOW);
+	err = gpio_pin_configure(gpio0_dev, 14, GPIO_OUTPUT_HIGH);
+	err = gpio_pin_configure(gpio0_dev, 15, GPIO_OUTPUT_HIGH);
+	err = gpio_pin_configure(gpio0_dev, 16, GPIO_OUTPUT_LOW);
 
 	/* Initialize diagnostics module. */
 #if CONFIG_DIAGNOSTICS
@@ -38,11 +53,13 @@ void main(void)
 		LOG_ERR("Event manager could not initialize.");
 	}
 	/* Initialize BLE module. */
-	if (ble_module_init()) {
-		LOG_ERR("Could not initialize BLE module");
-	}
+	//if (ble_module_init()) {
+	//	LOG_ERR("Could not initialize BLE module");
+	//}
 	/* Initialize firmware upgrade module. */
 	if (fw_upgrade_module_init()) {
 		LOG_ERR("Could not initialize firmware upgrade module");
 	}
+
+	
 }
