6b8c00030dedc05f917b9b413405ee3c05281549
eivind.haldorsen <eivind.haldorsen@inventas.no>
2022-01-28 00:43:38 -0800

    XF-25 add proper check of data contents

--- tests/storage_controller/src/storage_helper.c
+++ tests/storage_controller/src/storage_helper.c
@@ -8,8 +8,12 @@
 mem_rec cached_mem_rec_read;
 mem_rec cached_mem_rec_write;
 
+int write_index_value = 0;
+int read_index_value = 0;
+
 void request_data(flash_partition_t partition)
 {
+	k_sleep(K_SECONDS(1));
 	struct stg_read_memrec_event *ev = new_stg_read_memrec_event();
 
 	ev->new_rec = &cached_mem_rec_read;
@@ -20,7 +24,10 @@ void request_data(flash_partition_t partition)
 
 void consume_data(flash_partition_t partition)
 {
-	zassert_equal(cached_mem_rec_read.header.ID, 1337,
+	printk("Consuming real data %i, expects %i\n",
+	       cached_mem_rec_read.header.ID, read_index_value);
+
+	zassert_equal(cached_mem_rec_read.header.ID, read_index_value,
 		      "Contents not equal.");
 
 	/* After contents have been consumed successfully, 
@@ -30,12 +37,21 @@ void consume_data(flash_partition_t partition)
 	struct stg_data_consumed_event *ev = new_stg_data_consumed_event();
 	ev->partition = partition;
 	EVENT_SUBMIT(ev);
+
 	consumed_entries_counter++;
+	read_index_value++;
 }
 
 void write_data(flash_partition_t partition)
 {
-	cached_mem_rec_write.header.ID = 1337;
+	printk("Writes data %i\n", write_index_value);
+	cached_mem_rec_write.header.ID = write_index_value;
+
+	/* Everytime we write data, increment pointer so the ID changes,
+         * so we increment the read value for each read as well and rotate,
+         * which means the values should always be the same.
+         */
+	write_index_value++;
 
 	struct stg_write_memrec_event *ev = new_stg_write_memrec_event();
 	ev->new_rec = &cached_mem_rec_write;
