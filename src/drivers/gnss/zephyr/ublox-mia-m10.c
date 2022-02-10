1747037baf8fe6a85d62448049bb9fdfa6c4f827
Marius Lind Volstad <marius@vec.no>
2022-02-10 20:27:51 +0100

    XF-34 Basic UART handling and parsing thread created.

--- src/drivers/gnss/zephyr/ublox-mia-m10.c
+++ src/drivers/gnss/zephyr/ublox-mia-m10.c
@@ -2,6 +2,8 @@
 #define DT_DRV_COMPAT u_blox_mia_m10
 
 #include <init.h>
+#include <zephyr.h>
+#include <sys/ring_buffer.h>
 #include <drivers/gpio.h>
 #include <drivers/uart.h>
 #include <logging/log.h>
@@ -14,14 +16,19 @@ LOG_MODULE_REGISTER(MIA_M10, CONFIG_GNSS_LOG_LEVEL);
 #define GNSS_UART_NODE			DT_INST_BUS(0)
 #define GNSS_UART_DEV			DEVICE_DT_GET(GNSS_UART_NODE)
 
+/* RX thread structures */
+K_KERNEL_STACK_DEFINE(gnss_rx_stack,
+		      CONFIG_GNSS_MIA_M10_RX_STACK_SIZE);
+struct k_thread gnss_rx_thread;
 
-/* GNSS buffers for sending and receiving data. 
- * Mutex is used to protect buffers. 
-*/
+/* TODO - consider creating context struct */
+/* Semaphore for signalling thread about received data. */
+struct k_sem gnss_rx_sem;
+
+/* GNSS buffers for sending and receiving data. */
 static uint8_t* gnss_tx_buffer = NULL;
 static struct ring_buf gnss_tx_ring_buf;
 
-/* GNSS buffers for receiving data. */
 static uint8_t* gnss_rx_buffer = NULL;
 static struct ring_buf gnss_rx_ring_buf;
 
@@ -59,16 +66,23 @@ static void mia_m10_uart_flush(const struct device *uart_dev)
  */
 static void mia_m10_uart_handle_tx(const struct device *uart_dev)
 {
-	int bytes = 
-		uart_fifo_fill(uart_dev, 
-			       &gnss_tx_buffer[gnss_tx_sent], 
-			       gnss_tx_count - gnss_tx_sent);
-	gnss_tx_sent += bytes;
+	int err;
+	uint32_t size;
+	uint32_t sent;
+	uint8_t* data;
+
+	size = ring_buf_get_claim(&gnss_tx_ring_buf, 
+					&data, 
+					CONFIG_GNSS_MIA_M10_BUFFER_SIZE);
 	
-	if (gnss_tx_sent >= gnss_tx_count) {
-		atomic_clear_bit(&gnss_tx_in_progress, 0);
-		gnss_tx_count = 0;
-		gnss_tx_sent = 0;
+	sent = uart_fifo_fill(uart_dev, data, size);
+	
+	err = ring_buf_get_finish(&gnss_tx_ring_buf, sent);
+	if (err != 0) {
+		LOG_ERR("Failed finishing GNSS TX buffer operation.");
+	}
+	
+	if (ring_buf_is_empty(&gnss_tx_ring_buf)) {
 		uart_irq_tx_disable(uart_dev);
 	}
 }
@@ -79,23 +93,32 @@ static void mia_m10_uart_handle_tx(const struct device *uart_dev)
  * @param[in] uart_dev UART device to receive from. 
  */
 static void mia_m10_uart_handle_rx(const struct device *uart_dev)
-{	
+{
+	int ret;
 	uint8_t* data;
+
 	uint32_t partial_size = 
 		ring_buf_put_claim(&gnss_rx_ring_buf, 
-				   &data, 
-				   CONFIG_GNSS_MIA_M10_BUFFER_SIZE);
+				&data, 
+				CONFIG_GNSS_MIA_M10_BUFFER_SIZE);
 
 	if (partial_size == 0) {
-		passthrough_uart_flush(uart_dev);
+		mia_m10_uart_flush(uart_dev);
 		return;
 	}
 
 	uint32_t received = uart_fifo_read(uart_dev, 
-					   data, 
-					   partial_size);
+					data, 
+					partial_size);
 	
-	ring_buf_put_finish(&gnss_ring_buf, received);
+	ret = ring_buf_put_finish(&gnss_rx_ring_buf, received);
+	if (ret != 0) {
+		LOG_ERR("Failed finishing GNSS RX buffer operation.");
+	}
+
+	if (received > 0) {
+		k_sem_give(&gnss_rx_sem);
+	}
 }
 
 /**
@@ -116,6 +139,32 @@ static void mia_m10_uart_isr(const struct device *uart_dev,
 		if (uart_irq_rx_ready(uart_dev)) {
 			mia_m10_uart_handle_rx(uart_dev);
 		}
+	}
+}
+
+static void mia_m10_parse_rx(void)
+{
+	int err;
+	uint32_t size;
+	uint32_t sent;
+	uint8_t* data;
+
+	while (true) {
+		k_sem_take(&gnss_rx_sem, K_FOREVER);
+
+		size = ring_buf_get_claim(&gnss_rx_ring_buf, 
+						&data, 
+						CONFIG_GNSS_MIA_M10_BUFFER_SIZE);
+		
+		/* TODO - Parse received bytes */
+		sent = size; 
+		
+		err = ring_buf_get_finish(&gnss_rx_ring_buf, sent);
+		if (err != 0) {
+			LOG_ERR("Failed finishing GNSS RX buffer operation.");
+		}
+	
+		k_yield();
 	}
 }
 
@@ -144,6 +193,20 @@ static int mia_m10_init(const struct device *dev)
 			      CONFIG_GNSS_MIA_M10_BUFFER_SIZE, 
 			      gnss_rx_buffer);
 	}
+	if (gnss_tx_buffer == NULL)
+	{
+		gnss_tx_buffer = 
+			k_malloc(CONFIG_GNSS_MIA_M10_BUFFER_SIZE);
+
+		if (gnss_tx_buffer == NULL) {
+			return -ENOBUFS;
+		}
+		ring_buf_init(&gnss_tx_ring_buf, 
+			      CONFIG_GNSS_MIA_M10_BUFFER_SIZE, 
+			      gnss_tx_buffer);
+	}
+	
+	k_sem_init(&gnss_rx_sem, 0, 1);
 
 	/* Make sure interrupts are disabled */
 	uart_irq_rx_disable(uart_dev);
@@ -151,8 +214,14 @@ static int mia_m10_init(const struct device *dev)
 
 	/* Prepare UART for data reception */
 	mia_m10_uart_flush(uart_dev);
-	uart_irq_callback_set(uart_dev, passthrough_uart_isr);
+	uart_irq_callback_set(uart_dev, mia_m10_uart_isr);
 	uart_irq_rx_enable(uart_dev);
+	
+	/* Start RX thread */
+	k_thread_create(&gnss_rx_thread, gnss_rx_stack,
+			K_KERNEL_STACK_SIZEOF(gnss_rx_stack),
+			(k_thread_entry_t) mia_m10_parse_rx,
+			NULL, NULL, NULL, K_PRIO_COOP(7), 0, K_NO_WAIT);
 
 	return 0;
 }
