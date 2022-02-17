f283aea6cdde9ecb7a50182469924ebb29f6cba8
Daniel Svendsen <daniel.svendsen@inventas.no>
2022-02-17 12:51:27 +0100

    XF-78 Publish battery event with voltage upon periodic read

--- src/events/power_manager/pwr_event.h
+++ src/events/power_manager/pwr_event.h
@@ -11,13 +11,12 @@
 /** @brief Enum for different electric pulse status */
 enum pwr_state_flag { PWR_NORMAL = 0, PWR_LOW, PWR_CRITICAL, PWR_BATTERY };
 
-
-
 /** @brief Struct containg status messages regarding electric pulse events. */
 struct pwr_status_event {
 	struct event_header header;
 
 	enum pwr_state_flag pwr_state;
+	uint16_t battery_mv;
 };
 
 EVENT_TYPE_DECLARE(pwr_status_event);
