776db638b303215883e5986871e957bb5bc1cbd3
Marius Lind Volstad <marius@vec.no>
2022-02-21 09:51:19 +0100

    XF-34 Fixing GNSS received data handler consume. Fixing building without modem driver

--- src/drivers/gnss/zephyr/ublox-mia-m10.c
+++ src/drivers/gnss/zephyr/ublox-mia-m10.c
@@ -560,7 +560,7 @@ static uint32_t mia_m10_parse_data(uint8_t* buffer, uint32_t cnt)
 			/* NMEA */
 			uint32_t i = nmea_parse(
 					&buffer[parsed], (cnt-parsed));
-			if (parsed == 0) {
+			if (i == 0) {
 				/* Nothing parsed means not enough data yet */
 				break;
 			} else {
@@ -570,7 +570,7 @@ static uint32_t mia_m10_parse_data(uint8_t* buffer, uint32_t cnt)
 			/* UBLOX */
 			uint32_t i = ublox_parse(
 					&buffer[parsed], (cnt-parsed));
-			if (parsed == 0) {
+			if (i == 0) {
 				/* Nothing parsed means not enough data yet */
 				break;
 			} else {
