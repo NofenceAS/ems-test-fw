1301a91183c428acd11f65f5d05e3dbee532ffb7
eivind.haldorsen <eivind.haldorsen@inventas.no>
2022-02-23 03:17:39 -0800

    XF-110 add pasture unit test non-event driven

--- tests/storage_controller/src/storage_helper.h
+++ tests/storage_controller/src/storage_helper.h
@@ -7,88 +7,26 @@
 
 #include <ztest.h>
 #include "storage.h"
-#include "storage_events.h"
 
-void request_log_data();
-void request_ano_data();
-void request_pasture_data();
-
-void write_log_data();
-void write_ano_data();
-void write_pasture_data(uint8_t num_points);
-
-/** @brief Function that consumes data acquired from the ack read event.
- *         The ack read has a uint8_t* data pointer, as well as
- *         a size_t len field, which can be used to typecast to struct necessary
- *         based on partition.
- */
-void consume_data(struct stg_ack_read_event *event);
-
-extern uint8_t *write_log_ptr;
-extern uint8_t *write_ano_ptr;
-extern uint8_t *write_pasture_ptr;
-
-extern flash_partition_t cur_partition;
-
-extern struct k_sem write_log_ack_sem;
-extern struct k_sem read_log_ack_sem;
-extern struct k_sem consumed_log_ack_sem;
-
-extern struct k_sem write_ano_ack_sem;
-extern struct k_sem read_ano_ack_sem;
-extern struct k_sem consumed_ano_ack_sem;
-
-extern struct k_sem write_pasture_ack_sem;
-extern struct k_sem read_pasture_ack_sem;
-extern struct k_sem consumed_pasture_ack_sem;
-
-/** Variables used to check how many times we had to rotate FCB during write
- *  By this, we can estimate how many read operations (walk callbacks) will
- *  be triggered and put the numbers into zassert. We only have this for LOG
- *  since ANO and PASTURE is never full as we only care about the newest entry.
- * 
- *  Basically used by calculating how many entries are added when it's full.
- * 
- *  first_log_read is also included, which simply reads the value at flash
- *  so that we can expect all of the next reads to be +1.
- *  We need this because when we start to rotate/replace old entries before
- *  we're able to read them, we cannot verify the contents
- *  since we do not know the relation between write and read pointer on the FCB.
- */
-extern int num_new_entries;
-extern bool first_log_read;
-
-extern int log_write_index_value;
-extern int log_read_index_value;
-
-extern int ano_write_index_value;
-extern int ano_read_index_value;
-
+void fill_dummy_fence(void);
 /* Pasture tests. */
-void test_pasture_write(void);
-void test_pasture_read(void);
+void test_pasture(void);
 void test_pasture_extended_write_read(void);
 void test_reboot_persistent_pasture(void);
-void test_request_pasture_twice(void);
+void test_request_pasture_multiple(void);
+void test_no_pasture_available(void);
 
 /* Log tests. */
-void test_log_write(void);
-void test_log_read(void);
-void test_reboot_persistent_log(void);
-void test_write_log_exceed(void);
-void test_read_log_exceed(void);
-void test_empty_walk_log(void);
-
-/* Ano tests. */
-void test_ano_write(void);
-void test_ano_read(void);
-void test_reboot_persistent_ano(void);
-
-typedef enum {
-	TEST_ID_NORMAL = 0,
-	TEST_ID_MASS_LOG_WRITES = 1,
-	TEST_ID_PASTURE_DYNAMIC = 2
-} test_id_t;
-extern test_id_t cur_test_id;
+//void test_log_write(void);
+//void test_log_read(void);
+//void test_reboot_persistent_log(void);
+//void test_write_log_exceed(void);
+//void test_read_log_exceed(void);
+//void test_empty_walk_log(void);
+//
+///* Ano tests. */
+//void test_ano_write(void);
+//void test_ano_read(void);
+//void test_reboot_persistent_ano(void);
 
 #endif /* _STORAGE_HELPER_H_ */
--- tests/storage_controller/src/storage_test_log.c
+++ tests/storage_controller/src/storage_test_log.c
@@ -4,7 +4,6 @@
 
 #include <ztest.h>
 #include "storage.h"
-#include "storage_events.h"
 #include "storage_helper.h"
 
 #include "ano_structure.h"
@@ -14,9 +13,6 @@
 #include "pm_config.h"
 #include <stdlib.h>
 
-int num_new_entries = 0;
-bool rotated_log = false;
-
 /* Number of entries available from the partition we have emulated if no 
  * headers/paddings are added, which it is. We use this number to ensure
  * we exceed the partition size when we test to append too many entries.
@@ -25,49 +21,14 @@ bool rotated_log = false;
 
 void test_log_write(void)
 {
-	write_log_data();
-	int err = k_sem_take(&write_log_ack_sem, K_SECONDS(30));
-	zassert_equal(err, 0,
-		      "Test execution hanged waiting for write log ack.");
 }
 
 void test_log_read(void)
 {
-	/* First we request, then we get callback in event_handler when
-	 * data is ready. There might be multiple entries since
-	 * last read, so the event_handler can trigger several times
-	 * with new entries. We just have to take care of the
-	 * consume_event once we've consumed the data.
-	 */
-	request_log_data();
-	int err = k_sem_take(&read_log_ack_sem, K_SECONDS(30));
-	zassert_equal(err, 0,
-		      "Test execution hanged waiting for read log ack.");
-
-	/* We process the data further in event_handler. */
-
-	err = k_sem_take(&consumed_log_ack_sem, K_SECONDS(30));
-	zassert_equal(err, 0,
-		      "Test execution hanged waiting for consumed log ack.");
 }
 
 void test_write_log_exceed(void)
 {
-	/* We can calculate the minimum amount of entries needed
-	 * to fill up the sectors, so add 1 to exceed the limit and
-	 * expect the storage controller to rotate FCB since it returns
-	 * -ENOSPC.
-	 */
-	int num_log_sectors = LOG_ENTRY_COUNT + 1;
-
-	int err;
-
-	for (int i = 0; i < num_log_sectors; i++) {
-		write_log_data();
-		err = k_sem_take(&write_log_ack_sem, K_SECONDS(30));
-		zassert_equal(err, 0, "Test hanged waiting for write log ack.");
-		k_sleep(K_MSEC(50));
-	}
 }
 
 /** @brief Checks what happens if we try to read after there's a long time
@@ -75,74 +36,12 @@ void test_write_log_exceed(void)
 */
 void test_read_log_exceed(void)
 {
-	cur_test_id = TEST_ID_MASS_LOG_WRITES;
-	first_log_read = true;
-
-	int log_writes = num_new_entries - 1;
-
-	printk("There are %i new entries available.\n", log_writes);
-
-	int err;
-	/* Walk LOG data entries. */
-	request_log_data();
-
-	/* Reset FCB, pretending to reboot. Checks if persistent storage
-	 * works. Should get the expected read value from the written one above.
-	 */
-	for (int i = 0; i < log_writes; i++) {
-		err = k_sem_take(&consumed_log_ack_sem, K_SECONDS(30));
-		zassert_equal(
-			err, 0,
-			"Test hanged waiting for multiple consumed log ack.");
-		k_sleep(K_MSEC(100));
-	}
-	num_new_entries = 0;
-
-	cur_test_id = TEST_ID_NORMAL;
-
-	k_sleep(K_SECONDS(30));
 }
 
 void test_empty_walk_log(void)
 {
-	/* Expect semaphore to hang, since we do not get any callbacks due
-	 * to entries being consumed on fcb_walk().
-	 */
-	request_log_data();
-
-	int err = k_sem_take(&read_log_ack_sem, K_SECONDS(30));
-	zassert_not_equal(err, 0, "");
-	err = k_sem_take(&consumed_log_ack_sem, K_SECONDS(30));
-	zassert_not_equal(err, 0, "");
 }
 
 void test_reboot_persistent_log(void)
 {
-	int err;
-
-	/* Write 5 logs. */
-	for (int i = 0; i < 5; i++) {
-		write_log_data();
-		err = k_sem_take(&write_log_ack_sem, K_SECONDS(30));
-		zassert_equal(err, 0, "Test hanged waiting for write log ack.");
-		k_sleep(K_MSEC(50));
-	}
-
-	/* Reset FCB, pretending to reboot the device. Checks persistent storage
-	 * works. Should get the expected read value from the written one above.
-	 */
-
-	err = stg_fcb_reset_and_init();
-	zassert_equal(err, 0, "Error simulating reboot and FCB resets.");
-
-	/* Read something, expect 5 semaphores being given. */
-	request_log_data();
-
-	for (int i = 0; i < 5; i++) {
-		err = k_sem_take(&consumed_log_ack_sem, K_SECONDS(30));
-		zassert_equal(
-			err, 0,
-			"Test hanged waiting for multiple consumed log ack.");
-		k_sleep(K_MSEC(50));
-	}
 }
