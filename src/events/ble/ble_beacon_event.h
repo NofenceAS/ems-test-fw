362c33b0aeb4edd7529c5debc15bdecf70902970
Daniel Svendsen <daniel.svendsen@inventas.no>
2022-03-02 09:47:36 +0100

    XF-28 add hysteresis and send event

---
+++ src/events/ble/ble_beacon_event.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2021 Nofence AS
+ */
+
+#ifndef _BLE_BEACON_EVENT_H_
+#define _BLE_BEACON_EVENT_H_
+
+/**
+ * @brief BLE Beacon Event
+ * @defgroup ble_conn_event BLE Connection Event
+ * @{
+ */
+
+#include <string.h>
+
+#include "event_manager.h"
+
+enum beacon_status_type {
+	BEACON_STATUS_REGION_NEAR = 0,
+	BEACON_STATUS_REGION_FAR,
+	BEACON_STATUS_NOT_FOUND
+};
+
+/** BLE connection event. */
+struct ble_beacon_event {
+	struct event_header header;
+
+	enum beacon_status_type status;
+};
+
+EVENT_TYPE_DECLARE(ble_beacon_event);
+
+#endif /* _BLE_CONN_EVENT_H_ */
