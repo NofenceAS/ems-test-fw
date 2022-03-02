75faa22bd22dcb9dc90546e07754a4334bfac1ab
Marius Lind Volstad <marius@vec.no>
2022-03-02 13:39:54 +0100

    XF-65 Testing test by disabling BLE in main

--- tests-hw/nofence-tester/tests/X25/ble_scan.py
+++ tests-hw/nofence-tester/tests/X25/ble_scan.py
@@ -1,8 +1,10 @@
 def run(ble):
+    # TODO - Make device ID an environment variable
     device = ble.scan_for_device(addr="E9:F5:E1:40:45:A8")
 
     if device == None:
         raise Exception("Did not find device...")
     
     print("Found device " + str(device))
+    # TODO - Verify ScanData
     print("    ScanData " + str(device.getScanData()))
