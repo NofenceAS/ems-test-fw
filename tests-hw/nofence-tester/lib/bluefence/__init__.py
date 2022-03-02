9ea0aacf910c2d794c47a80c2431b739ce96f190
Marius Lind Volstad <marius@vec.no>
2022-03-02 13:30:09 +0100

    XF-65 Searching for specific BLE device

--- tests-hw/nofence-tester/lib/bluefence/__init__.py
+++ tests-hw/nofence-tester/lib/bluefence/__init__.py
@@ -5,26 +5,32 @@ from bluepy.btle import Scanner, DefaultDelegate, BTLEException
 import sys
 import time
 
-"""
-class ScanDelegate(DefaultDelegate):
-    def handleDiscovery(self, dev, isNewDev, isNewData):
-        print(strftime("%Y-%m-%d %H:%M:%S", gmtime()), dev.addr, dev.getScanData())
-        sys.stdout.flush()
-"""
-
-
 class BlueFence:
     def __init__(self):
         pass
     
-    def scan(self):
-        scanner = Scanner() #.withDelegate(ScanDelegate())
+    def scan_for_device(self, addr=None, uuid=None):
+        result_device = None
+        
+        scanner = Scanner()
 
         scanner.start(passive=True)
 
         start = time.time()
-        while (time.time()-start) < 10.0:
+        while (not result_device) and ((time.time()-start) < 10.0):
             scanner.process(0.25)
-            print(scanner.getDevices())
+            devices = scanner.getDevices()
+            for dev in devices:
+                match = True
+                if addr:
+                    if dev.addr.lower() != addr.lower():
+                        match = False
+                if uuid:
+                    # TODO - Check for UUID
+                    pass
+                if match:
+                    result_device = dev
 
         scanner.stop()
+
+        return result_device
--- tests-hw/nofence-tester/tests/X25/ble_scan.py
+++ tests-hw/nofence-tester/tests/X25/ble_scan.py
@@ -1,2 +1,4 @@
 def run(ble):
-    ble.scan()
+    device = ble.scan_for_device(addr="E9:F5:E1:40:45:A8")
+
+    print("Found device " + str(device))
