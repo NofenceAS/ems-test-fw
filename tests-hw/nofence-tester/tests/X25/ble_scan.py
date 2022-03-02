6addb0c971c47a3e1766c53fdc131b52892b9c22
Marius Lind Volstad <marius@vec.no>
2022-03-02 13:34:26 +0100

    XF-65 Printing scan data for found BLE device

--- tests-hw/nofence-tester/tests/X25/ble_scan.py
+++ tests-hw/nofence-tester/tests/X25/ble_scan.py
@@ -1,4 +1,8 @@
 def run(ble):
     device = ble.scan_for_device(addr="E9:F5:E1:40:45:A8")
 
+    if device == None:
+        raise Exception("Did not find device...")
+    
     print("Found device " + str(device))
+    print("    ScanData " + str(device.getScanData()))
