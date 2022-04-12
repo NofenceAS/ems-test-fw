5dc82382f676843e9a04e5044809669955a823d2
alaa <alaa@nofence.no>
2022-04-12 10:21:53 +0200

    add a message queue to allow messaging to access the histogram.

--- src/events/collar_stats/amc_events.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by alaa on 07.04.2022.
-//
-
-#include "amc_events.h"
--- src/events/collar_stats/amc_events.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by alaa on 07.04.2022.
-//
-
-#ifndef X3_FW_AMC_EVENTS_H
-#define X3_FW_AMC_EVENTS_H
-
-#endif //X3_FW_AMC_EVENTS_H
