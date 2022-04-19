e3c2435e75cac5e40c2cbc8972566f018fb531b0
Daniel Svendsen <daniel.svendsen@inventas.no>
2022-04-19 10:21:14 +0200

    XF-71 re-enable welcome sound

--- src/main.c
+++ src/main.c
@@ -128,9 +128,9 @@ void main(void)
 	}
 
 	/* Play welcome sound. */
-	// struct sound_event *sound_ev = new_sound_event();
-	// sound_ev->type = SND_WELCOME;
-	// EVENT_SUBMIT(sound_ev);
+	struct sound_event *sound_ev = new_sound_event();
+	sound_ev->type = SND_WELCOME;
+	EVENT_SUBMIT(sound_ev);
 
 	err = cellular_controller_init();
 	if (err) {
