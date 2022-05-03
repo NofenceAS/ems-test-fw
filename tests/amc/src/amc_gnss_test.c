da51a0795e484bdb1b5969516b6a02a042f0597a
eivind.haldorsen <eivind.haldorsen@inventas.no>
2022-05-03 01:42:59 -0700

    XF-143 fix gnss mode test

--- tests/amc/src/amc_gnss_test.c
+++ tests/amc/src/amc_gnss_test.c
@@ -15,6 +15,53 @@ static int gnss_timeout(void)
 	return 0;
 }
 
+static gnss_t gnss_data = { .latest = { .pvt_flags = 1 },
+			    .fix_ok = true,
+			    .lastfix = { .pvt_flags = 1 },
+			    .has_lastfix = true };
+
+void simulate_no_fix(void)
+{
+	memset(&gnss_data, 0, sizeof(gnss_data));
+
+	/* Send GNSS data, has fix, but not much more */
+	zassert_equal(gnss_update(&gnss_data), 0, "Failed updating GNSS fix");
+}
+
+void simulate_fix(void)
+{
+	memset(&gnss_data, 0, sizeof(gnss_data));
+	gnss_data.latest.pvt_flags = 1;
+	gnss_data.fix_ok = true;
+	gnss_data.lastfix.pvt_flags = 1;
+	gnss_data.has_lastfix = true;
+
+	/* Send GNSS data, has fix, but not much more */
+	zassert_equal(gnss_update(&gnss_data), 0, "Failed updating GNSS fix");
+}
+
+void simulate_easy_fix(void)
+{
+	simulate_fix();
+	gnss_data.latest.pvt_flags = 1;
+	gnss_data.latest.num_sv = 7;
+	gnss_data.latest.h_dop = 80;
+	gnss_data.latest.h_acc_dm = 65;
+	gnss_data.latest.height = 100;
+	zassert_equal(gnss_update(&gnss_data), 0, "Failed updating GNSS fix");
+}
+
+void simulate_accepted_fix(void)
+{
+	simulate_fix();
+	gnss_data.latest.pvt_flags = 1;
+	gnss_data.latest.num_sv = 7;
+	gnss_data.latest.h_dop = 80;
+	gnss_data.latest.h_acc_dm = 25;
+	gnss_data.latest.height = 100;
+	zassert_equal(gnss_update(&gnss_data), 0, "Failed updating GNSS fix");
+}
+
 void test_gnss_fix(void)
 {
 	k_sem_init(&gnss_fix_timeout, 0, 1);
@@ -22,13 +69,8 @@ void test_gnss_fix(void)
 	zassert_equal(gnss_init(gnss_timeout), 0,
 		      "Failed initializing AMC GNSS");
 
-	gnss_t gnss_data = { .latest = { .pvt_flags = 1 },
-			     .fix_ok = true,
-			     .lastfix = { .pvt_flags = 1 },
-			     .has_lastfix = true };
-
 	/* Send GNSS data, has fix, but not much more */
-	zassert_equal(gnss_update(&gnss_data), 0, "Failed updating GNSS fix");
+	simulate_fix();
 
 	/* Verify state of fix */
 	zassert_true(gnss_has_fix(), "Did not have fix");
@@ -53,12 +95,7 @@ void test_gnss_fix(void)
 	zassert_false(gnss_has_easy_fix(), "Did have easy fix");
 
 	/* Easy fix */
-	gnss_data.latest.pvt_flags = 1;
-	gnss_data.latest.num_sv = 7;
-	gnss_data.latest.h_dop = 80;
-	gnss_data.latest.h_acc_dm = 65;
-	gnss_data.latest.height = 100;
-	zassert_equal(gnss_update(&gnss_data), 0, "Failed updating GNSS fix");
+	simulate_easy_fix();
 
 	/* Verify state of easy fix */
 	zassert_true(gnss_has_fix(), "Did not have fix");
@@ -66,12 +103,7 @@ void test_gnss_fix(void)
 	zassert_true(gnss_has_easy_fix(), "Did not have easy fix");
 
 	/* Accepted fix */
-	gnss_data.latest.pvt_flags = 1;
-	gnss_data.latest.num_sv = 7;
-	gnss_data.latest.h_dop = 80;
-	gnss_data.latest.h_acc_dm = 25;
-	gnss_data.latest.height = 100;
-	zassert_equal(gnss_update(&gnss_data), 0, "Failed updating GNSS fix");
+	simulate_accepted_fix();
 
 	/* Verify state of accepted fix */
 	zassert_true(gnss_has_fix(), "Did not have fix");
@@ -94,32 +126,70 @@ static gnss_mode_t current_gnss_mode = GNSSMODE_INACTIVE;
 
 void test_gnss_mode(void)
 {
-	/* Caution state with unknown. */
-	FenceStatus fs = FenceStatus_FenceStatus_UNKNOWN;
-	CollarStatus cs = CollarStatus_CollarStatus_UNKNOWN;
-	Mode mode = Mode_Mode_UNKNOWN;
+	/* Inactive mode, set first_time_since_start also due to simulate_easy_fix. 
+	 * to not receive GNSSMODE_CAUTION.
+	 */
+	simulate_easy_fix();
+
+	FenceStatus fs = FenceStatus_FenceStatus_Normal;
+	CollarStatus cs = CollarStatus_Sleep;
+	Mode mode = Mode_Teach;
 	amc_zone_t zone = NO_ZONE;
+
+	set_sensor_modes(mode, fs, cs, zone);
+	zassert_equal(k_sem_take(&gnss_mode_sem, K_SECONDS(30)), 0, "");
+	zassert_equal(current_gnss_mode, GNSSMODE_INACTIVE, "");
+
+	/* Caution mode. */
+	simulate_no_fix();
+
+	fs = FenceStatus_FenceStatus_UNKNOWN;
+	cs = CollarStatus_CollarStatus_UNKNOWN;
+	mode = Mode_Mode_UNKNOWN;
+	zone = NO_ZONE;
 
 	set_sensor_modes(mode, fs, cs, zone);
 	zassert_equal(k_sem_take(&gnss_mode_sem, K_SECONDS(30)), 0, "");
 	zassert_equal(current_gnss_mode, GNSSMODE_CAUTION, "");
 
+	/* PSM mode 1, PSM zone. */
+	simulate_easy_fix();
+
+	fs = FenceStatus_FenceStatus_Normal;
+	cs = CollarStatus_CollarStatus_Normal;
+	mode = Mode_Teach;
+	zone = PSM_ZONE;
+
+	set_sensor_modes(mode, fs, cs, zone);
+	zassert_equal(k_sem_take(&gnss_mode_sem, K_SECONDS(30)), 0, "");
+	zassert_equal(current_gnss_mode, GNSSMODE_PSM, "");
+
 	/* MAX mode. */
-	//fs = FenceStatus_FenceStatus_Normal;
-	//cs = CollarStatus_CollarStatus_Normal;
-	//mode = Mode_Teach;
-	//zone = WARN_ZONE;
-	//
-	//set_sensor_modes(mode, fs, cs, zone);
-	//zassert_equal(k_sem_take(&gnss_mode_sem, K_SECONDS(30)), 0, "");
-	//printk("%i", current_gnss_mode);
-	//zassert_equal(current_gnss_mode, GNSSMODE_MAX, "");
+	fs = FenceStatus_FenceStatus_Normal;
+	cs = CollarStatus_CollarStatus_Normal;
+	mode = Mode_Teach;
+	zone = WARN_ZONE;
+
+	set_sensor_modes(mode, fs, cs, zone);
+	zassert_equal(k_sem_take(&gnss_mode_sem, K_SECONDS(30)), 0, "");
+	zassert_equal(current_gnss_mode, GNSSMODE_MAX, "");
+
+	/* PSM mode 2, escaped. */
+	simulate_accepted_fix();
+
+	fs = FenceStatus_Escaped;
+	cs = CollarStatus_CollarStatus_Normal;
+	mode = Mode_Teach;
+	zone = NO_ZONE;
+
+	set_sensor_modes(mode, fs, cs, zone);
+	zassert_equal(k_sem_take(&gnss_mode_sem, K_SECONDS(30)), 0, "");
+	zassert_equal(current_gnss_mode, GNSSMODE_PSM, "");
 }
 
 static bool event_handler(const struct event_header *eh)
 {
 	if (is_gnss_set_mode_event(eh)) {
-		printk("Event handler");
 		struct gnss_set_mode_event *ev = cast_gnss_set_mode_event(eh);
 		current_gnss_mode = ev->mode;
 		k_sem_give(&gnss_mode_sem);
--- tests/amc/src/amc_test.c
+++ tests/amc/src/amc_test.c
@@ -146,8 +146,8 @@ void test_main(void)
 	ztest_test_suite(amc_zone_tests, ztest_unit_test(test_zone_calc));
 	ztest_run_test_suite(amc_zone_tests);
 
-	ztest_test_suite(amc_gnss_tests, ztest_unit_test(test_gnss_mode),
-			 ztest_unit_test(test_gnss_fix));
+	ztest_test_suite(amc_gnss_tests, ztest_unit_test(test_gnss_fix),
+			 ztest_unit_test(test_gnss_mode));
 	ztest_run_test_suite(amc_gnss_tests);
 }
 
