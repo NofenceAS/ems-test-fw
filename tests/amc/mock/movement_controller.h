4fa36d41bee2593afc97aba1733ecdde7a8cd46a
eivind.haldorsen <eivind.haldorsen@inventas.no>
2022-05-20 00:18:14 -0700

    XF-157 add mock for movement controller

---
+++ tests/amc/mock/movement_controller.h
@@ -0,0 +1,9 @@
+
+#include <zephyr.h>
+#ifndef MOVEMENT_CONTROLLER_MOCK_H
+#define MOVEMENT_CONTROLLER_MOCK_H
+
+/** @brief Used to get the delta of how long we've been in active state. */
+uint32_t get_active_delta(void);
+
+#endif //MOVEMENT_CONTROLLER_MOCK_H
