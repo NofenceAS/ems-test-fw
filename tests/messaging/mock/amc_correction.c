cc880f6c309ecc9260bb4cd021105a18ce7ddc2d
Daniel Svendsen <daniel.svendsen@inventas.no>
2022-09-06 15:35:34 +0200

    XF-138 remove unused function, and test mock files

--- tests/messaging/mock/amc_correction.c
+++
@@ -1,7 +0,0 @@
-#include <ztest.h>
-#include "amc_correction.h"
-
-int16_t get_last_fence_dist(void)
-{
-	return ztest_get_return_value();
-}
--- tests/messaging/mock/amc_correction.h
+++
@@ -1,7 +0,0 @@
-#include <zephyr.h>
-#ifndef X3_AMC_CORRECTION_MOCK_H
-#define X3_AMC_CORRECTION_MOCK_H
-
-int16_t get_last_fence_dist(void);
-
-#endif // X3_AMC_CORRECTION_MOCK_H
