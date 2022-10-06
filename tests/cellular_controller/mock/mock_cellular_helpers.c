a76446f3ba1d959ae87b70dbcb825edbab171bc6
alaa <alaa@nofence.no>
2022-10-06 15:27:37 +0200

    add tests for cc and messaging, plus some minor fixes here and there

--- tests/cellular_controller/mock/mock_cellular_helpers.c
+++ tests/cellular_controller/mock/mock_cellular_helpers.c
@@ -47,8 +47,8 @@ int8_t send_tcp(char *dummy, size_t dummy_len)
 
 int send_tcp_q(char *dummy, size_t dummy_len)
 {
-	ARG_UNUSED(dummy);
-	ARG_UNUSED(dummy_len);
+	ztest_check_expected_value(*dummy);
+	ztest_check_expected_value(dummy_len);
 	return ztest_get_return_value();
 }
 
