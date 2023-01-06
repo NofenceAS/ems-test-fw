a906f973f69be448a45028f12a5d1e55d0fc399c
jorgenrh <jorgenhoem@gmail.com>
2023-01-06 21:38:17 +0100

    diag working buzzer and pulse

--- src/modules/diagnostics/commander/cmd_stimulator.c
+++ src/modules/diagnostics/commander/cmd_stimulator.c
@@ -105,21 +105,34 @@ int commander_stimulator_handler(enum diagnostics_interface interface, uint8_t c
 		}
 		break;
 	}
-	case BUZZER_WARN: {
-		/** @todo Warning frequency as argument? */
+	case BUZZER_WARN:
+	{	
+		uint32_t freq = WARN_FREQ_MAX;
 
-		/* Simulate the highest tone event */
+		if (size >= 4) {
+			freq = (data[0] << 0) + 
+					(data[1] << 8) + 
+					(data[2] << 16) + 
+					(data[3] << 24);
+		}
+
 		struct sound_event *sound_event_warn = new_sound_event();
 		sound_event_warn->type = SND_WARN;
 		EVENT_SUBMIT(sound_event_warn);
 
+		/** unable to confirm frequency
+S			 * <Timeout on getting a new warn zone freq>, Error code=-116, Sever~ */
 		struct sound_set_warn_freq_event *sound_warn_freq = new_sound_set_warn_freq_event();
-		sound_warn_freq->freq = WARN_FREQ_MAX;
-		EVENT_SUBMIT(sound_warn_freq);
+		sound_warn_freq->freq = freq;
+		EVENT_SUBMIT(sound_warn_freq); 
+
+		resp = DATA;
+		commander_send_resp(interface, STIMULATOR, cmd, resp, (uint8_t*)&freq, sizeof(uint32_t));
 
 		break;
 	}
-	case ELECTRICAL_PULSE: {
+	case ELECTRICAL_PULSE:
+	{
 		struct warn_correction_pause_event *ev_q_test_zap =
 			new_warn_correction_pause_event();
 		ev_q_test_zap->reason = Reason_WARNPAUSEREASON_ZAP;
