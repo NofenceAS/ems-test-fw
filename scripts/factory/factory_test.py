6468b8fe759f3590b139c90bebacebb65c37121d
Leif Gunnar <leif.gunnar@nofence.no>
2023-01-11 14:55:13 +0100

    update factory_test.py

--- scripts/factory/factory_test.py
+++ scripts/factory/factory_test.py
@@ -1,5 +1,11 @@
 import sys
+import re
+import subprocess
 import os
+import time
+import random
+from threading import Thread
+
 base_path = os.path.dirname(os.path.abspath(__file__))
 lib_path = os.path.join(base_path, "..")
 sys.path.append(lib_path)
@@ -9,15 +15,19 @@ import signal
 import time
 import argparse
 import struct
+from collections import namedtuple
 
 # Parse input arguments
 parser = argparse.ArgumentParser(description='Nofence GNSS recorder')
 parser.add_argument('--ble', help='Serial number of device in advertised name of device for BLE communication')
+parser.add_argument('--comport', help='Serial comport connected to the BLE uart gateway', required=False)
 parser.add_argument('--rtt', help='Serial number of Segger J-Link to use for RTT communication')
 parser.add_argument('--jlinkpath', help='Path to JLink executable')
-parser.add_argument('--suid', help='DUT Unique serial number', required=True)
-parser.add_argument('--pt', help='DUT Product type', required=True)
-parser.add_argument('--ems', help='DUT EMS provider', required=True)
+parser.add_argument('--suid', help='DUT Unique serial number', required=False)
+parser.add_argument('--pt', help='DUT Product type', required=False)
+parser.add_argument('--ems', help='DUT EMS provider', required=False)
+parser.add_argument('--server', help='DUT server to connect to', required=True)
+parser.add_argument('--flash', help='If DUT should be flashed or not', required=True)
 args = parser.parse_args()
 
 # Build connection
@@ -26,12 +36,55 @@ if (args.ble and args.rtt):
 elif (not args.ble) and (not args.rtt):
 	print("Didn't specify a connection. RTT will be used on any available J-Link devices.")
 
-#print(args.suid + " will be flashed into DUT")
-#print(args.pt + " will be flashed into DUT")
+
+def flash_mcu(file, verbose=True) -> bool:
+    cmd = ['nrfjprog', '--program', file, '--chiperase', '--verify', '--reset']
+    if verbose: 
+        print(' '.join(cmd))
+    process = subprocess.Popen(
+		cmd, 
+		stdout=subprocess.PIPE, 
+		stderr=subprocess.PIPE
+	)
+    out, err = map(lambda res: re.sub('[\r\n]+', '\r', res.decode('utf-8', 'ignore').strip()).split('\r'), process.communicate()) 
+
+    if verbose: 
+        for o in out: print(o)
+    error = any('error' in e.lower() for e in err)
+    if error or verbose: 
+        for e in err: print(e)
+    process.terminate()
+    return not error
+
+if not args.suid:
+	while 1:
+		suid = input("Enter serial number: ")
+		if suid and len(suid) > 5:
+			args.suid = suid.strip()
+			break
+		else:
+			print(f'invalid serial number {suid}')
+
+
+set_file = open(args.suid + "_" + str(int(time.time())) + ".log", "w")
+
+if (args.flash == "y"):
+	#file = 'C:\\Nofence\\firmware\\x3\\x3-fw-app\\merged_with_text_parser.hex'	
+	file = '.\\merged.hex'
+	print("Start flashing...")
+	flash_ok = flash_mcu(file)
+	print(flash_ok)
+	set_file.write("SeggerFLASH = "  + str(flash_ok) + "\n")
+	print("End")
+	time.sleep(3);
+else:
+	set_file.write("SeggerFLASH = Not Flashed" + "\n")
+	print("Not flashed")
 
 stream = None
 if args.ble:
-	stream = nfdiag.BLEStream("/dev/ttyACM1", serial=args.ble)
+	#stream = nfdiag.BLEStream("COM4", serial=args.ble)
+	stream = nfdiag.BLEStream(port=args.comport, serial=args.ble)
 else:
 	stream = nfdiag.JLinkStream(serial=args.rtt, jlink_path=args.jlinkpath)
 
@@ -54,7 +107,7 @@ def signal_handler(sig, frame):
 	del stream
 
 	sys.exit(0)
-signal.signal(signal.SIGINT, signal_handler)
+#signal.signal(signal.SIGINT, signal_handler)
 
 # Sending ping to verify connection
 print("Trying to ping...")
@@ -70,23 +123,33 @@ if not got_ping:
 
 # Write settings 
  # TODO: What values?
-if args.rtt:
+
+if (args.suid):
 	if not cmndr.write_setting(nfdiag.ID_SERIAL, int(args.suid)):
 		raise Exception("Failed to write settings")
-	if not cmndr.write_setting(nfdiag.ID_HOST_PORT, b"172.31.36.11:4321\x00"):
-		raise Exception("Failed to write settings")
+if (args.ems):
 	if not cmndr.write_setting(nfdiag.ID_EMS_PROVIDER, int(args.ems)):
 		raise Exception("Failed to write settings")
-	if not cmndr.write_setting(nfdiag.ID_PRODUCT_RECORD_REV, 2):
-		raise Exception("Failed to write settings")
-	if not cmndr.write_setting(nfdiag.ID_BOM_MEC_REV, 5):
-		raise Exception("Failed to write settings")
-	if not cmndr.write_setting(nfdiag.ID_BOM_PCB_REV, 2):
-		raise Exception("Failed to write settings")
-	if not cmndr.write_setting(nfdiag.ID_HW_VERSION, 20):
-		raise Exception("Failed to write settings")
+if (args.pt):
 	if not cmndr.write_setting(nfdiag.ID_PRODUCT_TYPE, int(args.pt)):
 		raise Exception("Failed to write settings")
+
+if(args.server=="Production"):
+	if not cmndr.write_setting(nfdiag.ID_HOST_PORT, b"172.31.33.243:9876\x00"):
+		raise Exception("Failed to write settings")
+elif(args.server=="Staging"):	
+	if not cmndr.write_setting(nfdiag.ID_HOST_PORT, b"172.31.36.11:4321\x00"):
+		raise Exception("Failed to write settings")	
+else:
+	raise Exception("Unknown servername")	
+if not cmndr.write_setting(nfdiag.ID_PRODUCT_RECORD_REV, 2):
+	raise Exception("Failed to write settings")
+if not cmndr.write_setting(nfdiag.ID_BOM_MEC_REV, 5):
+	raise Exception("Failed to write settings")
+if not cmndr.write_setting(nfdiag.ID_BOM_PCB_REV, 4):
+	raise Exception("Failed to write settings")
+if not cmndr.write_setting(nfdiag.ID_HW_VERSION, 20):		#20 = HW_Q Started a new range when making new HW with nRF52840
+	raise Exception("Failed to write settings")
 
 print("1: Serial No: " + str(cmndr.read_setting(nfdiag.ID_SERIAL)))
 print("2: HOST PORT: " + str(cmndr.read_setting(nfdiag.ID_HOST_PORT)))
@@ -129,33 +192,14 @@ if resp:
 else:
 	raise Exception("No response when issuing test command")
 
-print("Giving a pulse in 5 seconds!!!!!!!!!!")
-time.sleep(5)
-val = cmndr.electric_pulse_now()
 
-while 1:
-	x = input("Press p to give a pulse!")
-	if x == "p":
-		print("Giving a pulse in 2 seconds!!!!!!!!!!")
-		time.sleep(2)
-		cmndr.electric_pulse_now()
-	elif x == "x":
-		print("Finished test!!")
-		break
+#print("Start GNSS output each 2sec")
 
-# Read CCID from modem
-ccid = b""
-timeout = time.time()+15
-while len(ccid) == 0:
-	ccid = cmndr.modem_get_ccid()
-	if ccid is None:
-		raise Exception("No response while reading CCID")
-	if time.time() > timeout:
-		raise Exception("Timed out waiting for valid CCID. Modem failure!")
-print("Modem CCID: " + str(int(ccid)))
+#gnss_struct_t = namedtuple('gnss_struct_t',['num_sv','pvt_flags','pvt_valid','overflow','x','y','height','speed','head_veh','h_dop','h_acc_dm','v_acc_dm','head_acc','lat','lon','updated_at','msss','ttff'])
+
 
 # Read settings
-set_file = open(args.suid + "_" + str(int(time.time())) + ".log", "w")
+#set_file = open(args.suid + "_" + str(int(time.time())) + ".log", "w")
 
 val = cmndr.read_setting(nfdiag.ID_SERIAL)
 if val is None:
@@ -197,11 +241,137 @@ if val is None:
 	raise Exception("Failed to read settings")
 set_file.write("ID_PRODUCT_TYPE = " + str(val) + "\n")
 
-set_file.write("CCID = " + str(int(ccid)) + "\n")
+buzzer_on = False
+ep_on = False
+output_on = False
+
+def play_buzzer():
+	while True:
+		if buzzer_on:
+			cmndr.start_buzzer()
+			time.sleep(2)
+
+def infinit_ep():
+	zapcnt = 0
+	while True:
+		if ep_on:
+			zapcnt = zapcnt + 1
+			print("ZAP=" + str(zapcnt) + "\n")
+			time.sleep(0.2)
+			cmndr.electric_pulse_now()
+			time.sleep(5.8)
+
+oneshot = True
+def output_data():
+	while True:
+		if output_on:
+			if oneshot == True:
+				set_file.write("UID - numSv,cno1,cno2,cno3,cno4,flags,valid,ovf,height,speed,hdop,lat,lon,msss,ttff,vbatt,isolar,vint,acc_x,acc_y,acc_z,temp,pres,hum"+ "\n")
+				print("UID - numSv,cno1,cno2,cno3,cno4,flags,valid,ovf,height,speed,hdop,lat,lon,msss,ttff,vbatt,isolar,vint,acc_x,acc_y,acc_z,temp,pres,hum"+"\n")
+			oneshot = False			
+		elif output_on == False:
+			oneshot = True	
+
+		if output_on:
+			try:		
+				time.sleep(1)
+				onboard_data = cmndr.get_onboard_data()
+
+				value = struct.unpack("BBBBBBBBhHHiiIIIIIiiiiii", onboard_data["data"][0:68])
+				print(str(cmndr.read_setting(nfdiag.ID_SERIAL)) + " - " + "{0}".format(value))
+				set_file.write(str(cmndr.read_setting(nfdiag.ID_SERIAL)) + " - " + "{0}".format(value) + "\n")
+
+			except KeyboardInterrupt:
+				print("Loop stopped")
+				set_file.write("Loop stopped" + "\n")
+				break
+			except Exception as e:
+				print(f'error: {e}')			
+
+buzzer_thread = Thread(target=play_buzzer, args=())
+buzzer_thread.daemon = True
+buzzer_thread.start()
+
+infinit_ep_thread = Thread(target=infinit_ep, args=())
+infinit_ep_thread.daemon = True
+infinit_ep_thread.start()
+
+output_thread = Thread(target=output_data, args=())
+output_thread.daemon = True
+output_thread.start()
+
+while 1:
+	print("Press following to run test")
+	print("'z' - Start single EP test")
+	print("'Z' - Start infinit EP test")
+	print("'I' - Start infinit EP test runtime")
+	print("'O' - Output debug data")
+	print("'s' - Toggle 1V8_S voltage")
+	print("'c' - Toggle onoff charging")
+	print("'a' - Start EP and Toggle onoff charging")
+	print("'b' - Start buzzer continous tone")
+	print("'Thread1' - Start/Stop Cellular Thread")
+	print("'TH1' - Start Cellular Thread")
+	print("'TH2' - Stop Cellular Thread")
+	x = input("'x' jump to next test step -> ")
+	if x == "z":
+		print("Pulse in 1 seconds!!!!!!!!!!")
+		time.sleep(1)
+		cmndr.electric_pulse_now()	
+	elif x == "Z":
+		print("Infinit EP each 5 second!!!!!!!!!!")
+		ep_on = not ep_on
+	elif x == "I":
+		cmndr.electric_pulse_infinite(10)			
+	elif x == "b":
+		buzzer_on = not buzzer_on			
+	elif x == "c":
+		cmndr.turn_onoff_charging()	
+	elif x == "s":
+		cmndr.turn_onoff_1v8s()		
+	elif x == "O":		
+		output_on = not output_on		
+	elif x == "a":
+		print("Pulse in 1 seconds!!!!!!!!!!")
+		time.sleep(1)
+		cmndr.electric_pulse_now()		
+		time.sleep(1)
+		cmndr.turn_onoff_charging()	
+	elif x == "TH1":		
+		print("Start cellular thread")
+		cmndr.thread_control(1)	
+	elif x == "TH2":		
+		print("Stop cellular thread")
+		cmndr.thread_control(0)			
+	elif x == "x":		
+		break
+			
+# Read CCID from modem
+ccid = b""
+timeout = time.time()+60
+while len(ccid) == 0:
+	ccid = cmndr.modem_get_ccid()
+	if ccid is None:
+		raise Exception("No response while reading CCID")		
+	if time.time() > timeout:
+		raise Exception("Timed out waiting for valid CCID. Modem failure!")
+print("Modem CCID: " + str(int(ccid)))
+set_file.write("Modem CCID = " + str(int(ccid)) + "\n")
+
+while 1:
+	x = input("Testing OK? 'y' or 'n'")
+	if x == "y":
+		print("Testeds OK!!" + "\n")
+		set_file.write("Testeds OK!!" + "\n")
+		break
+	elif x == "n":
+		print("Test FAILED!!" + "\n")
+		set_file.write("Test FAILED!!" + "\n")
+		break
 
 set_file.close()
 
-print("Success!")
-
+#cleanup_stop_thread()
+sys.exit()
 # Testing done, tear down
 signal_handler(None, None)
