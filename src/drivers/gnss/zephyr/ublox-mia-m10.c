ce5071fa8489573d1665d28c18cfc54b54628e64
Fabien Lepoutre <fabien@cionic.com>
2023-01-16 11:31:09 +0100

    fixes

--- src/drivers/gnss/zephyr/ublox-mia-m10.c
+++ src/drivers/gnss/zephyr/ublox-mia-m10.c
@@ -442,14 +442,14 @@ static int mia_m10_setup(const struct device *dev, bool try_default_baud_first)
 
 #if (CONFIG_GNSS_MIA_M10_FIXMODE != 0)
 	/* 3D fix only */
-	ret = mia_m10_config_set_u8(UBX_CFG_FIXMODE, UBX_CFG_FIXMODE_3D);
+	ret = mia_m10_config_set_u8(UBX_CFG_FIXMODE, CONFIG_GNSS_MIA_M10_FIXMODE);
 	if (ret != 0) {
 		return ret;
 	}
 #endif
 #if (CONFIG_GNSS_MIA_M10_PACC != 0)
 	/* 50m output accuracy*/
-	ret = mia_m10_config_set_u16(UBX_CFG_OUTFIL_PACC, 50);
+	ret = mia_m10_config_set_u16(UBX_CFG_OUTFIL_PACC, CONFIG_GNSS_MIA_M10_PACC);
 	if (ret != 0) {
 		return ret;
 	}
